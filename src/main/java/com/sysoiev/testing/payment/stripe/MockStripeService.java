45a3c465664c2243a4d21dec7d65d6b60d8b25c4
Bada1208 <sysoievbogdan@gmail.com>
2021-02-15 10:00:38 +0100

    add stripe

---
+++ src/main/java/com/sysoiev/testing/payment/stripe/MockStripeService.java
@@ -0,0 +1,25 @@
+package com.sysoiev.testing.payment.stripe;
+
+import com.sysoiev.testing.payment.CardPaymentCharge;
+import com.sysoiev.testing.payment.CardPaymentCharger;
+import com.sysoiev.testing.payment.Currency;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@ConditionalOnProperty(
+        value = "stripe.enabled",
+        havingValue = "false"
+)
+public class MockStripeService implements CardPaymentCharger {
+    @Override
+    public CardPaymentCharge chargeCard(String cardSource,
+                                        BigDecimal amount,
+                                        Currency currency,
+                                        String description) {
+        return new CardPaymentCharge(true);
+    }
+}
